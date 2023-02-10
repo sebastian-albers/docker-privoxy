6dea2633f19623892a7c21f0972ddb2dbf4af3b3
Sebastian Albers <seb.albers@gmail.com>
2023-02-10 09:26:40 +0100

    Updated workflow to use main branch.

--- read-version.sh
+++ read-version.sh
@@ -2,5 +2,6 @@
 
 set -eou pipefail
 
-docker run sebastianalbers/privoxy:latest-$1 --version |
-  sed '/^Privoxy version/!{q1}; s/^Privoxy version \([0-9\.]*\) .*$/\1/'
+docker run --rm sebastianalbers/privoxy:latest-$1 --version > version.tmp
+sed '/^Privoxy version/!{q1}; s/^Privoxy version \([0-9\.]*\) .*$/\1/' version.tmp
+rm version.tmp
